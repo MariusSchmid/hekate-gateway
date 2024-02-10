cebf84abfbe1f83efdf4782053cad2bd152c3d9e
Marius <mafloschmid@aol.com>
2024-02-10 16:27:55 +0100

    can build for unix and rp2

---
+++ build-mpy-rp2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cmake -B build
+cmake --build build
+cd external/micropython
+make -C ports/rp2 submodules
+make -C mpy-cross
+cd ports/rp2
+make USER_C_MODULES=../../../../mpy-modules/
