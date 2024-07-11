181365bca46820db160d68a8405f474bb2a4e6c4
Marius Schmid <mafloschmid@aol.com>
2024-07-11 20:15:33 +0200

    remove hekatelib

--- software/hekatelib/include/hekate.h
+++
@@ -1,9 +0,0 @@
-#ifndef D495C8C8_25BC_4911_B86C_A348225BC066
-#define D495C8C8_25BC_4911_B86C_A348225BC066
-
-#include <stdbool.h>
-
-#include "hekate_handles.h"
-
-bool hekate_init(hekate_hal_t hekate_hal);
-#endif /* D495C8C8_25BC_4911_B86C_A348225BC066 */
