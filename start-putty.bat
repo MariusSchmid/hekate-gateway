ac652d1aca1efdb4f9e913cf8cc31e5b2f6edc72
Marius Schmid <mafloschmid@aol.com>
2024-07-20 07:52:12 +0000

    bat scripts for simpler development

---
+++ start-putty.bat
@@ -0,0 +1,4 @@
+:start
+plink -serial COM18:115200
+timeout /t 2
+goto start
---
+++ start-software-dev.bat
@@ -0,0 +1,6 @@
+usbipd attach --wsl --hardware-id  2e8a:000c 
+
+wsl -e bash -lic "cd /home/marius/development/repos/hekate-gateway/software && code ."
+
+@REM TODO: start in container
+@REM TODO: unsafe gits
